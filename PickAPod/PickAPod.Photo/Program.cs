1dce24e07059dde7533e0ed813a84e3121306265
Mandy <fullstackmandurlorian@gmail.com>
2025-01-24 15:03:14 -0500

    fixed build error on photo microservice

--- PickAPod/PickAPod.Photo/Program.cs
+++ PickAPod/PickAPod.Photo/Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddHttpClient();
 builder.Services.AddScoped<IPhotoService, PhotoService>();
 
 var app = builder.Build();
