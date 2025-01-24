1fa7d6f371bd7bb9f46b6bf73ed60186ee853caf
Mandy <fullstackmandurlorian@gmail.com>
2025-01-24 15:13:55 -0500

    Photo service corrections

--- PickAPod/PickAPod.Photo/Program.cs
+++ PickAPod/PickAPod.Photo/Program.cs
@@ -1,3 +1,5 @@
+using PickAPod.Photo.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
--- PickAPod/PickAPod.Photo/Services/PhotoService.cs
+++ PickAPod/PickAPod.Photo/Services/PhotoService.cs
@@ -1,6 +1,40 @@
 ﻿namespace PickAPod.Photo.Services
 {
-    public class PhotoService
+    public class PhotoService : IPhotoService
     {
+        private readonly HttpClient _httpClient;
+        private readonly string _nasaApiKey;
+        private readonly ILogger<PhotoService> _logger;
+
+        public PhotoService(HttpClient httpClient, IConfiguration configuration, ILogger<PhotoService> logger)
+        {
+            _httpClient = httpClient;
+            _nasaApiKey = configuration["NasaApiKey"];
+            _logger = logger;
+        }
+
+        public async Task<string> GetAPODForDate(string date)
+        {
+            var url = $"https://api.nasa.gov/planetary/apod?api_key={_nasaApiKey}&date={date}";
+            var response = await _httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public async Task<string> GetAPODs(string startDate, string endDate)
+        {
+            var url = $"https://api.nasa.gov/planetary/apod?api_key={_nasaApiKey}&start_date={startDate}&end_date={endDate}";
+            var response = await _httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public async Task<string> SearchAPOD(string query)
+        {
+            var url = $"https://api.nasa.gov/planetary/apod?api_key={_nasaApiKey}&q={query}";
+            var response = await _httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
     }
 }
