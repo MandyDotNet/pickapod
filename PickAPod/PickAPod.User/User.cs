3485a049298db6f9373eb60f8786e5a081226161
Mandy <fullstackmandurlorian@gmail.com>
2025-02-13 12:10:30 -0500

    updates to user gitignore

---
+++ PickAPod/PickAPod.User/User.cs
@@ -0,0 +1,9 @@
+namespace PickAPod.User
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ PickAPod/PickAPod.User/UserManagement.cs
@@ -0,0 +1,15 @@
+﻿namespace PickAPod.User
+{
+    public class UserManagement
+    {
+        private User _user;
+        public User GetUser()
+        {
+            return _user;
+        }
+        public void SetUser(User user)
+        {
+            _user = user;
+        }
+    }
+}
